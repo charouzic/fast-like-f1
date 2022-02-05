87f3c509d664bfaa71fe4f0e5f817371a23edb5e
Viktor <vmac@itu.dk>
2022-02-05 10:44:28 +0100

    Adding table creation and Preprocessor file

---
+++ src/F1.DbUp/Scripts/Script0001 - Create tables.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA READ;
+
+CREATE TABLE read.drivers
+(
+    id integer,
+    firstName CHARACTER VARYING(50),
+    lastName CHARACTER VARYING(50),
+    country CHARACTER VARYING(50),
+    dateOfBirth timestamp,
+    placeOfBirth CHARACTER VARYING(50),
+    team CHARACTER VARYING(50),
+    numberOfPodiums integer,
+    firstSeason integer,
+    note CHARACTER VARYING(200)
+);
